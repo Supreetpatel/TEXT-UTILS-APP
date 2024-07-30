ba8eb22c820665f1e7ef27cf4b0fda184abd73a6
Supreet0 <150177964+Supreetpatel@users.noreply.github.com>
2024-07-30 11:28:21 +0530

    one

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -15,10 +15,6 @@ export default function Navbar(props) {
           <a class="nav-link active" aria-current="page" to="/">Home</a>
         </li>
       </ul>
-      {/*<form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-primary" type="submit">Search</button>
-      </form>*/}
       <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
       <input className="form-check-input" onClick={props.toggleMode}  type="checkbox" role="switch" id="flexSwitchCheckDefault" />
       <label className="form-check-label" htmlForor="flexSwitchCheckDefault">Dark mode</label>
